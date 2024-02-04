f5b93f55ce6031fc22ab9fe6441542aadd11519d
Michael <michael.michael@bankmandiri.co.id>
2024-02-04 21:10:37 +0700

    test ssh key

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const skill = ['Javascript', 'Golang']
 console.log ('Nama Lengkap | Umur | Alamat  | Skills')
 console.log ('------------ | ---- | ------  | ------')
 console.log ('    Fadli    |  22  | Ragunan | Javascript, Golang')
+console.log ('test ssh key')
